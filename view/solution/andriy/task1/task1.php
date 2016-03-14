73186634551bbbd6fbae81b168a1ad002a30cc97
bitko <y.bitko@bigmir.net>
2016-03-14 14:42:31 +0200

    Добавив себе

--- view/solution/andriy/task1/task1.php
+++ view/solution/andriy/task1/task1.php
@@ -1,16 +1,16 @@
+<?php
+include '.\view\solution\andriy\task1\action.php';
+?>
+
+
 <div class="content">
     <form action="" method = "post">
         <div> Введіть логін</br><input type="text" name="login"></div>
         <div> Введіть пароль</br><input type="password" name="pass"></div>
-        <div><input type="submit" name="sub" value="&#1058;&#1080;&#1089;&#1085;&#1080;"</br></div>
+        <div><input type="submit" name="sub" value="Тисни"</br></div>
+    </form>
+    <p><?php echo @($login) ?></p>
+    <p><?php echo @(@$pass) ?></p>
 
-        <?php
-        if(isset($_POST['login'], $_POST['pass'])) {
-            echo '<p>Ви ввели логін: ' . $_POST['login'];
-            '</p>';
-            echo '<p>Ви ввели пароль: ' . $_POST['pass'];
-            '</p';
-        }
-        ?>
 
 </div>
