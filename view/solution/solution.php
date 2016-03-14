fbc2dfa0431cee5e40c60b62543273d8892d3505
bitko <y.bitko@bigmir.net>
2016-03-14 14:41:39 +0200

    Добавив себе

--- view/solution/solution.php
+++ view/solution/solution.php
@@ -1,6 +1,7 @@
 <div class="content">
     <h3><a href="index.php?page=view/solution/andriy/userTasks">Андрій</a></br></h3>
     <h3><a href="index.php?page=view/solution/vitaliy/userTasks">Віталій</a></h3>
+    <h3><a href="index.php?page=view/solution/yuriy/userTasks">Юрій</a></h3>
 
     <p>Завдання виконуються у теці testphp/view/solution/"ім'я користувача"/"номер завдання"</br>
     Файл task1.php - це файл який мыстить html код (файл вигляду). Всі інші файли створюються у цій теці.</br>
---
+++ view/solution/yuriy/userTasks.php
@@ -0,0 +1,4 @@
+<div class="content">
+    <h3><a href="index.php?page=view/solution/yuriy/task1/task1">Тестове завдання №1</a></br></h3>
+
+</div>
