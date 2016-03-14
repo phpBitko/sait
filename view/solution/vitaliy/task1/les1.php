a43287bc1e9b1b7667d19e8949fd48e799200bf6
phpBitko <y.bitko@bigmir.net>
2016-03-14 15:24:52 +0200

    Є

--- view/solution/vitaliy/task1/les1.php
+++ view/solution/vitaliy/task1/les1.php
@@ -1,11 +1,6 @@
 ﻿<?php
-
 if (isset($_POST['valName'],$_POST['valPassword'])) {
     $nameVal = $_POST['valName'];
     $pasVal = $_POST['valPassword'];
 }
-echo "Ім'я - " .$_POST['valName'];
-echo "<br />";
-echo "Пароль - ". $_POST['valPassword'];
-exit;
 ?>
