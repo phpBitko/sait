de8143b91fc3db16f1ffec1233ead41d92d06e4a
phpBitko <y.bitko@bigmir.net>
2016-03-23 15:26:01 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 class Core{
-	static $DSN = 'mysql:dbname=php_sait;host=localhost';
-	static $USER ='root' ;
-	static $PASSWORD = '';
+	static $DSN = 'mysql:dbname=u419221701_myphp;host=http://myphp.hol.es';
+	static $USER ='u419221701_myphp' ;
+	static $PASSWORD = 'bitko12345';
 }
