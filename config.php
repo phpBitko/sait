4935c20899d45b9f34dff5b9498fa450708661d3
phpBitko <y.bitko@bigmir.net>
2016-03-23 15:37:09 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 class Core{
-	static $DSN = 'mysql:dbname=u419221701_myphp;host=http://myphp.hol.es';
+	static $DSN = 'mysql:dbname=u419221701_myphp;host=mysql.hostinger.com.ua';
 	static $USER ='u419221701_myphp' ;
 	static $PASSWORD = 'bitko12345';
 }
