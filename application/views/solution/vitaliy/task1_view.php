81aaadf8c481edc6aeff6185e32491d4be2c5c2a
bitko <y.bitko@bigmir.net>
2016-03-28 19:09:12 +0300

    Добавлено адмін панель

--- application/views/solution/vitaliy/task1_view.php
+++ application/views/solution/vitaliy/task1_view.php
@@ -1,9 +1,9 @@
 <?php
 //namespace application\views\solution\vitaliy;
 
-include 'task1\task1_model.php';
-include 'task1\task1_controller.php';
-include 'task1\task1_view_local.php';
+include 'task1/task1_model.php';
+include 'task1/task1_controller.php';
+include 'task1/task1_view_local.php';
 
 
 
